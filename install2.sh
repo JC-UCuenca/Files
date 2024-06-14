8a38cdacb7e6e61f7014cb4916ed685749673bb5
JC-UCuenca <121994103+JC-UCuenca@users.noreply.github.com>
2024-06-14 14:13:49 -0500

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,9 @@
+sudo apt install -y python-is-python3 \
+libboost-all-dev \
+gcc g++ pkg-config libasound2-dev libgtk2.0-dev \
+libsndfile1-dev libcurl4-nss-dev libarchive-dev liblo-dev libtag1-dev \
+vamp-plugin-sdk librubberband-dev libfftw3-dev libaubio-dev libxml2-dev \
+lv2-dev libserd-dev libsord-dev libsratom-dev liblilv-dev libgtkmm-2.4-dev \
+libglibmm-2.4-dev libusb-1.0-0-dev libpangomm-1.4-dev libsamplerate0-dev \
+libcunit1-dev libcppunit-dev libudev-dev libwebsockets-dev \
+libsratom-0-0 liblrdf0 liblrdf0-dev libsuil-dev
