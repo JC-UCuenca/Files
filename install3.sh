0702401159825142b25d986f08fdfd5f5f3d8f36
JC-UCuenca <121994103+JC-UCuenca@users.noreply.github.com>
2024-06-14 14:29:13 -0500

    Create install3.sh

---
+++ install3.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# Instalar python-is-python3
+sudo apt install -y python-is-python3
+
+# Instalar libboost-all-dev
+sudo apt install -y libboost-all-dev
+
+# Instalar gcc
+sudo apt install -y gcc
+
+# Instalar g++
+sudo apt install -y g++
+
+# Instalar pkg-config
+sudo apt install -y pkg-config
+
+# Instalar libasound2-dev
+sudo apt install -y libasound2-dev
+
+# Instalar libgtk2.0-dev
+sudo apt install -y libgtk2.0-dev
+
+# Instalar libsndfile1-dev
+sudo apt install -y libsndfile1-dev
+
+# Instalar libcurl4-nss-dev
+sudo apt install -y libcurl4-nss-dev
+
+# Instalar libarchive-dev
+sudo apt install -y libarchive-dev
+
+# Instalar liblo-dev
+sudo apt install -y liblo-dev
+
+# Instalar libtag1-dev
+sudo apt install -y libtag1-dev
+
+# Instalar vamp-plugin-sdk
+sudo apt install -y vamp-plugin-sdk
+
+# Instalar librubberband-dev
+sudo apt install -y librubberband-dev
+
+# Instalar libfftw3-dev
+sudo apt install -y libfftw3-dev
+
+# Instalar libaubio-dev
+sudo apt install -y libaubio-dev
+
+# Instalar libxml2-dev
+sudo apt install -y libxml2-dev
+
+# Instalar lv2-dev
+sudo apt install -y lv2-dev
+
+# Instalar libserd-dev
+sudo apt install -y libserd-dev
+
+# Instalar libsord-dev
+sudo apt install -y libsord-dev
+
+# Instalar libsratom-dev
+sudo apt install -y libsratom-dev
+
+# Instalar liblilv-dev
+sudo apt install -y liblilv-dev
+
+# Instalar libgtkmm-2.4-dev
+sudo apt install -y libgtkmm-2.4-dev
+
+# Instalar libglibmm-2.4-dev
+sudo apt install -y libglibmm-2.4-dev
+
+# Instalar libusb-1.0-0-dev
+sudo apt install -y libusb-1.0-0-dev
+
+# Instalar libpangomm-1.4-dev
+sudo apt install -y libpangomm-1.4-dev
+
+# Instalar libsamplerate0-dev
+sudo apt install -y libsamplerate0-dev
+
+# Instalar libcunit1-dev
+sudo apt install -y libcunit1-dev
+
+# Instalar libcppunit-dev
+sudo apt install -y libcppunit-dev
+
+# Instalar libudev-dev
+sudo apt install -y libudev-dev
+
+# Instalar libwebsockets-dev
+sudo apt install -y libwebsockets-dev
+
+# Instalar libsratom-0-0
+sudo apt install -y libsratom-0-0
+
+# Instalar liblrdf0
+sudo apt install -y liblrdf0
+
+# Instalar liblrdf0-dev
+sudo apt install -y liblrdf0-dev
+
+# Instalar libsuil-dev
+sudo apt install -y libsuil-dev
